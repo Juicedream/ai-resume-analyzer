3b9dd8481186ced3e0fd0e8f7719ecd4016ef94d
Juicedream <judexfrayo@gmail.com>
2025-11-13 14:25:19 +0100

    Form creation for resume upload

---
+++ app/components/FileUploader.tsx
@@ -0,0 +1,78 @@
+import React, {useCallback, useState} from 'react'
+import {useDropzone} from 'react-dropzone'
+import {formatSize} from "~/lib/utils";
+
+interface FileUploaderProps {
+    onFileSelect: (file: File | null) => void;
+}
+
+const FileUploader = ({onFileSelect}: FileUploaderProps) => {
+    const [file, setFile] = useState<File | null>(null);
+
+    const onDrop = useCallback((acceptedFiles: File[]) => {
+        const newFile = acceptedFiles[0] || null;
+        setFile(newFile);
+        onFileSelect?.(newFile);
+    }, [onFileSelect])
+
+    const maxFileSize = 20 * 1024 * 1024;
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone(
+        {
+            onDrop,
+            multiple: false,
+            accept: {"application/pdf": [".pdf"]},
+            maxSize: maxFileSize,
+        }
+    )
+
+    const handleRemove = (e: React.MouseEvent) => {
+        e.preventDefault();
+        e.stopPropagation();
+        setFile(null);
+        onFileSelect?.(null);
+    };
+
+    return (
+        <div className="w-full gradient-border">
+            <div {...getRootProps()}>
+                <input {...getInputProps()} />
+                <div className="space-y-4 cursor-pointer">
+                    {
+                        file ? (
+                            <div className="uploader-selected-file" onClick={(e) => e.stopPropagation()}>
+                                <img src="/images/pdf.png" alt="pdf image" className="size-10"/>
+                                <div className="flex items-center space-x-3">
+                                    <div>
+                                        <p className={"text-sm font-medium text-gray-700 truncate max-w-xs"}>
+                                            {file.name}
+                                        </p>
+                                        <p className="text-sm text-gray-500">
+                                            {formatSize(file.size)}
+                                        </p>
+                                    </div>
+                                </div>
+                                <button className="p-2 cursor-pointer" onClick={handleRemove}>
+                                    <img src="/icons/cross.svg" alt="remove icon" className="w-4 h-4"/>
+                                </button>
+                            </div>
+                        ) : (
+                            <div>
+                                <div className="mx-auto w-16 h-16 flex items-center justify-center mb-2">
+                                    <img src="/icons/info.svg" alt="upload" className="size-20"/>
+                                </div>
+                                <p className="text-lg text-gray-500">
+                                    <span className="font-semibold">
+                                        Click to upload
+                                    </span> or drag and drop
+                                </p>
+                                <div className="text-lg text-gray-500">PDF (max {formatSize(maxFileSize)})</div>
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+}
+export default FileUploader
---
+++ app/lib/utils.ts
@@ -0,0 +1,20 @@
+/**
+ *@param bytes
+ * @returns
+ */
+export function formatSize(bytes: number): string {
+    // Handle edge cases
+    if (bytes === 0) return '0 Bytes';
+    if (bytes < 0) return 'Invalid size';
+
+    const units: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const k: number = 1024;
+
+    // Calculate the appropriate unit
+    const i: number = Math.floor(Math.log(bytes) / Math.log(k));
+
+    // Convert and format to 2 decimal places
+    const size: number = bytes / Math.pow(k, i);
+
+    return `${size.toFixed(2)} ${units[i]}`;
+}
