778dd32c5e909ef18e67224c67ab7a0b2b7884ee
Juicedream <judexfrayo@gmail.com>
2025-11-14 08:12:56 +0100

    Cloud storage, PDF-image conversion, key-value storage and AI feedback

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -18,3 +18,5 @@ export function formatSize(bytes: number): string {
 
     return `${size.toFixed(2)} ${units[i]}`;
 }
+
+export const generateUUID = () => crypto.randomUUID()
--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,8 +1,15 @@
 import Navbar from "~/components/Navbar";
 import {type FormEvent, useState} from "react";
 import FileUploader from "~/components/FileUploader";
+import {usePuterStore} from "~/lib/puter";
+import {useNavigate} from "react-router";
+import {convertPdfToImage} from "~/lib/pdfToImage";
+import {generateUUID} from "~/lib/utils";
+import {prepareInstructions} from "../../constants";
 
 const Upload = () => {
+    const {auth, fs, isLoading, ai, kv} = usePuterStore();
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
     const [file, setFile] = useState<File | null>(null);
@@ -11,21 +18,89 @@ const Upload = () => {
     const handleFileSelect = (file: File | null) => {
         setFile(file);
     }
-    const handleAnalyze = async ({}) => {
+    const handleAnalyze = async ({companyName, jobTitle, jobDescription, file}: {
+        companyName: string,
+        jobTitle: string,
+        jobDescription: string,
+        file: File
+    }) => {
+       try {
+           setIsProcessing(true);
+           setStatusText('Uploading the file....');
 
+           const uploadedFile = await fs.upload([file]);
+
+           if (!uploadedFile) {
+               setStatusText("Error: Failed to upload file");
+               setIsProcessing(false);
+               return;
+           }
+
+           setStatusText("Converting to image...");
+
+           const imageFile = await convertPdfToImage(file);
+
+           if(imageFile.error){
+               console.log("PDF conversion error: ", imageFile.error)
+               setStatusText(`Error: ${imageFile.error}`);
+               setIsProcessing(false);
+               return;
+           }
+
+           if (!imageFile.file) return setStatusText("Error: Failed to convert PDF to image");
+
+           setStatusText("Uploading the image...");
+
+           const uploadedImage  = await fs.upload([imageFile.file]);
+           if (!uploadedImage) return setStatusText("Error: Failed upload to image");
+           setStatusText("Preparing data....")
+
+           const uuid = generateUUID()
+           const data = {
+               id: uuid,
+               resumePath: uploadedFile.path,
+               imagePath: uploadedImage.path,
+               companyName,
+               jobTitle, jobDescription,
+               feedback: "",
+           }
+
+           await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+           setStatusText("Analyzing data...");
+
+           const feedback = await ai.feedback(
+               uploadedFile.path, prepareInstructions({jobTitle, jobDescription})
+           );
+
+           if(!feedback) return setStatusText("Error: Failed to analyze resume");
+
+           const feedbackText = typeof feedback.message.content === "string" ? feedback.message.content : feedback.message.content[0].text;
+
+           data.feedback = JSON.parse(feedbackText);
+
+           await kv.set(`resume:${uuid}`, JSON.stringify(data));
+
+           setStatusText("Analyzing complete, redirecting...");
+
+           console.log({data})
+       }catch(err){
+        console.error("Error in handleAnalyze function:", err);
+        setIsProcessing(false);
+       }
     }
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const form = e.currentTarget.closest("form");
-        if(!form) return;
+        if (!form) return;
         const formData = new FormData(form);
-        
-        const companyName = formData.get("company-name")
-        const jobTitle = formData.get("job-title")
-        const jobDescription = formData.get("job-description")
 
-        if(!file) return;
+        const companyName = formData.get("company-name") as string
+        const jobTitle = formData.get("job-title") as string
+        const jobDescription = formData.get("job-description") as string;
 
+        if (!file) return;
+        handleAnalyze({companyName, jobTitle, jobDescription, file})
 
     }
 
@@ -50,7 +125,7 @@ const Upload = () => {
 
                     {
                         !isProcessing && (
-                            <form id="upload-form" onSubmit={handleSubmit} className="f;lex flex-col gap-4 mt-8">
+                            <form id="upload-form" onSubmit={handleSubmit} className="flex flex-col gap-4 mt-8">
                                 <div className="form-div">
                                     <label htmlFor="company-name">
                                         Company Name
@@ -67,13 +142,14 @@ const Upload = () => {
                                     <label htmlFor="job-description">
                                         Job Description
                                     </label>
-                                    <textarea rows={5} name="job-description" id="job-description" placeholder="Job Description"/>
+                                    <textarea rows={5} name="job-description" id="job-description"
+                                              placeholder="Job Description"/>
                                 </div>
                                 <div className="form-div">
                                     <label htmlFor="uploader">
                                         Upload Resume
                                     </label>
-                                   <FileUploader onFileSelect={handleFileSelect}/>
+                                    <FileUploader onFileSelect={handleFileSelect}/>
                                 </div>
 
                                 <button className="primary-button" type="submit"> Analyze Resume</button>
